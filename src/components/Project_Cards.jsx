bd41b9c71d888b131337d31afab4d315f1e7d7ac
Nikita Sidarovich <niksi312@student.liu.se>
2026-09-23 23:45:09 +0200

    Restructured

--- src/components/Project_Cards.jsx
+++ src/components/Project_Cards.jsx
@@ -10,41 +10,57 @@ function Project_Cards({image_name}){
     {/*Project Description*/}
     let tag_array = []
 
+    {/*If flex is present the project has a demo, otherwise hidden is present*/}
+    let has_demo
+
     let youtube_link
+
+    let card_class
+
     switch (image_name) {
         case "Logos":
             im_path = "assets/Logos.png"
             project_description = "Logos is a natural language processing system that allows the user to translate natural language intor formal logic, both with and without the use of Large Language Models. This project was my master thesis work."
             tag_array = ["SWI-Prolog", "Answer Set Programming", "NLTK", "CFG", "LLM", "Flask"]
             youtube_link = ""
+            has_demo = "hidden w-fit ml-5 my-auto mb-5 content-end"
+            card_class = "flex flex-col mx-2 w-155 xl:h-180 rounded-3xl border-4 my-4 bg-red-950 border-red-600"
             break;
         case "Salify":
             im_path = "assets/Salify.png"
             project_description = "A project where sentiment analysis is performed on song lyrics with BERT. The analysis is performed on public Spotify playlist where the React front-end receives the sentiment scores from the Python backend using Flask."
             tag_array = ["Python","React","Flask", "Scikit", "BERT", "Spotify API", "GENIUS API"]
             youtube_link = ""
+            has_demo = "hidden w-fit ml-5 my-auto mb-5 content-end"
+            card_class = "flex flex-col mx-2 w-155 xl:h-180 rounded-3xl border-4 my-4 bg-red-950 border-red-600"
             break;
         case "TBMI26":
             im_path = "assets/TBMI26.png"
             project_description = "Lab work from the course: Neural Networks and Learning Systems (Linköping University). The labs covered supervised, deep and reinforcement learning. Boosting and Ensemble methods were also covered."
             tag_array = ["Python", "Machine Learning", "Supervised Learning", "Deep Learning", "Neural Networks", "Boosting", "Reinforcement Learning", "Ensemble"]
             youtube_link = "https://youtu.be/116fqyLzfzw"
+            has_demo = "hidden w-fit ml-5 my-auto mb-5 content-end"
+            card_class = "flex flex-col mx-2 w-155 xl:h-180 rounded-3xl border-4 my-4 bg-red-950 border-red-600"            
             break;
         case "Learn-To-Sing":
             im_path = "assets/Learn-To-Sing.png"
             project_description = "Learn To Sing was a hobby project developed for fun. It is a system that allows users to complete exercises and AI generated lessons to learn to sing. Users can also create accounts, which are stored in Firebase."
             tag_array = ["TypeScript", "AI", "Gemini API", "React", "Tailwind", "Motion", "Tensorflow", "Firebase", "WebSockets"]
             youtube_link = "https://youtu.be/Az4szO6rnE0"
+            has_demo = "flex w-fit ml-5 my-auto mb-5 content-end"
+            card_class = "flex flex-col mx-2 w-155 xl:h-180 rounded-3xl border-4 my-4 bg-red-950 border-red-600"
             break;
         case "Alpha-Spike":
             im_path = "assets/Alpha-Spike.png"
             project_description = "Alpha-Spike was a summerjobb project for 2 students, built for Linköping University. The system is a front-end webb app built to communicate with UTM-City, a drone simulation software built by the University. "
             tag_array = ["Python", "Flask", "SWI-Prolog", "Answer Set Programming", "A*", "Behaviour Trees", "Voice Interaction", "Docker", "ZMQ"]
             youtube_link = "https://www.youtube.com/watch?v=uOzF8TFDkdU"
+            has_demo = "flex w-fit ml-5 my-auto mb-5 content-end"
+            card_class = "flex flex-col mx-2 w-155 xl:h-180 rounded-3xl border-4 my-4 bg-red-950 border-red-600"
             break;
         }
     return(
-    <div key={image_name} class="flex flex-col mx-2 w-155 xl:h-200 rounded-3xl border-4 my-4 bg-red-950 border-red-600">
+    <div key={image_name} class={card_class}>
 
         <h1 class="text-3xl font-bold text-white mx-auto text-center mt-5" >{image_name}</h1>
         
@@ -60,7 +76,7 @@ function Project_Cards({image_name}){
 
         <motion.a whileHover={{ scale: 1.2 }}
         whileTap={{ scale: 0.95}}
-        transition={easeInOut} href= {youtube_link} target="_blank" class="flex w-fit ml-5 my-auto mb-5 content-end">
+        transition={easeInOut} href= {youtube_link} target="_blank" class={has_demo}>
             <img src="assets/youtube.png" class="w-14"></img>
             <p class="text-2xl ml-2 my-auto text-white">Demo</p>
         </motion.a>
